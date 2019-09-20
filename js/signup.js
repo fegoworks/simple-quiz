e32474be5dc297a042c2069f581d9a8d370d53f2
unknown <fegoworks@hotmail.com>
2019-09-20 02:32:00 +0100

    bug(fix): fix existing  user authentication at signup

--- js/signup.js
+++ js/signup.js
@@ -1,29 +1,38 @@
 const username = document.querySelector("#username")
-const email = document.querySelector("#email")
-const phone = document.querySelector("#phone")
-const password = document.querySelector("#password")
+
 const btnSignup = document.querySelector("form")
 
 console.log("app is running ")
 const DATABASE_URI = "http://localhost:3000/users"
 
 btnSignup.addEventListener('submit', async (event) => {
+  const email = document.querySelector("#email")
+  const phone = document.querySelector("#phone")
+  const password = document.querySelector("#password")
   event.preventDefault();
 
-  await fetch(DATABASE_URI, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      username: username.value,
-      email: email.value,
-      userrole: "user",
-      password: password.value,
-      phone: phone.value
-    })
-  });
+  const response = await fetch(`${DATABASE_URI}?email=${email.value}`);
+  const [user] = await response.json();
 
-  window.location.assign('../index.html')
+  if (user) {
+    alert("This email already exists. Please use another")
+  } else {
+    await fetch(DATABASE_URI, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username.value,
+        email: email.value,
+        userrole: "user",
+        password: password.value,
+        phone: phone.value
+      })
+    });
+
+    window.location.assign('../index.html')
+  }
+
 });
